1aafe5af9c9cda9c817395ea014478c6e953e659
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2023-10-09 09:27:38 -0300

    instalando phpinit via composer.phar

--- app_carrinho_compras/vendor/composer/autoload_real.php
+++ app_carrinho_compras/vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInitd312e2026aa6bdf91267f79d227dbfa4
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitd312e2026aa6bdf91267f79d227dbfa4::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
